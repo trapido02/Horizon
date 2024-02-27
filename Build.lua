9e496523dc86a1034c767f86ed00cd344f0420ac
trapido02 <trapido02.2@gmail.com>
2024-02-27 15:51:01 +0100

    Initial commit

---
+++ Build.lua
@@ -0,0 +1,17 @@
+-- premake5.lua
+workspace "Horizon"
+   architecture "x64"
+   configurations { "Debug", "Release", "Dist" }
+   startproject "App"
+
+   -- Workspace-wide build options for MSVC
+   filter "system:windows"
+      buildoptions { "/EHsc", "/Zc:preprocessor", "/Zc:__cplusplus" }
+
+OutputDir = "%{cfg.system}-%{cfg.architecture}/%{cfg.buildcfg}"
+
+group "Engine"
+	include "Engine/Build-Engine.lua"
+group ""
+
+include "Editor/Build-Editor.lua"
---
+++ Editor/Source/Editor.cpp
@@ -0,0 +1,6 @@
+#include "Engine/Engine.h"
+
+int main()
+{
+	Engine::PrintHelloWorld();
+}
