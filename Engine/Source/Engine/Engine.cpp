bf25a041cadfc76af97322025d294ca2801b7d2b
trapido02 <trapido02.2@gmail.com>
2024-02-27 23:26:40 +0100

    Added more GLFW window hints

--- Engine/Source/Engine/Engine.cpp
+++ Engine/Source/Engine/Engine.cpp
@@ -18,6 +18,10 @@ namespace Engine {
         if (!glfwInit())
             return;
 
+        glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
+        glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 6);
+        glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
         window = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);
         if (!window)
         {
